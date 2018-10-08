7922cd47539b7d91c6390d435722274312200972
Adrien Suau <adrien.suau@grenoble-inp.org>
2018-10-08 13:44:09 +0200

    Initialise the tests directory.
    This directory will host all the unit-tests of the project. It is
    initialised with:
      - An __init__.py file.
      - A "launch_all_tests.py" file that will launch all the tests in the
        directory recursively.
      - A "QTestCase.py" file that extends the base casse unittest.TestCase
        in order to provide more asserts, especially asserts related to
        maths (matrix norms, floating point "equality", ...).

---
+++ tests/QTestCase.py
@@ -0,0 +1,84 @@
+# ======================================================================
+# Copyright CERFACS (October 2018)
+# Contributor: Adrien Suau (suau@cerfacs.fr)
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding  by the  rules of  distribution of free software. You can use,
+# modify  and/or  redistribute  the  software  under  the  terms  of the
+# CeCILL-B license as circulated by CEA, CNRS and INRIA at the following
+# URL "http://www.cecill.info".
+#
+# As a counterpart to the access to  the source code and rights to copy,
+# modify and  redistribute granted  by the  license, users  are provided
+# only with a limited warranty and  the software's author, the holder of
+# the economic rights,  and the  successive licensors  have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using, modifying and/or  developing or reproducing  the
+# software by the user in light of its specific status of free software,
+# that  may mean  that it  is complicated  to manipulate,  and that also
+# therefore  means that  it is reserved for  developers and  experienced
+# professionals having in-depth  computer knowledge. Users are therefore
+# encouraged  to load and  test  the software's  suitability as  regards
+# their  requirements  in  conditions  enabling  the  security  of their
+# systems  and/or  data to be  ensured and,  more generally,  to use and
+# operate it in the same conditions as regards security.
+#
+# The fact that you  are presently reading this  means that you have had
+# knowledge of the CeCILL-B license and that you accept its terms.
+# ======================================================================
+
+"""Base class for all the tests of qtoolkit module."""
+
+import unittest
+
+import numpy
+
+import qtoolkit.maths.matrix.su2.operator_norm as op_norm
+import qtoolkit.utils.types as qtypes
+
+
+class QTestCase(unittest.TestCase):
+    """Base class for all the tests of qtoolkit module."""
+
+    def assert2NormClose(self, array1, array2, rtol: float = 1e-5,
+                         atol: float = 1e-8) -> None:
+        """Check if the two arrays are close to each other.
+
+        The check is performed by computing the 2-norm of their
+        element-wise difference and check that this norm is below a
+        given tolerence.
+
+        :param array1: First array.
+        :param array2: Second array.
+        :param rtol: Relative tolerance. See numpy.isclose documentation
+        for a more detailed explanation.
+        :param atol: Absolute tolerance. See numpy.isclose documentation
+        for a more detailed explanation.
+        """
+        self.assertTrue(
+            numpy.isclose(numpy.linalg.norm(array1 - array2), 0, rtol=rtol,
+                          atol=atol))
+
+    def assertOperatorNormClose(self, U: qtypes.UnitaryMatrix,
+                                V: qtypes.UnitaryMatrix, rtol: float = 1e-5,
+                                atol: float = 1e-8) -> None:
+        """Check if the two unitary matrices are close to each other.
+
+        The check is performed by computing the operator norm of the
+        difference of the two matrices and check that this norm is below
+        a given tolerence.
+
+        :param U: First array.
+        :param V: Second array.
+        :param rtol: Relative tolerance. See numpy.isclose documentation
+        for a more detailed explanation.
+        :param atol: Absolute tolerance. See numpy.isclose documentation
+        for a more detailed explanation.
+        """
+        message = (f"Matrices U = \n{U}\nand V = \n{V}\nare not close "
+                   f"enough! ||U-V|| = {op_norm.operator_norm(U - V)}.")
+        self.assertTrue(
+            numpy.isclose(op_norm.operator_norm(U - V), 0, rtol=rtol,
+                          atol=atol), msg=message)
