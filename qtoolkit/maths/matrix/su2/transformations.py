f707bac8f807dfd79c7455b1ca9b427f85bf1d7d
Adrien Suau <adrien.suau@grenoble-inp.org>
2018-10-12 14:47:32 +0200

    Implementation of transformations between SU(2), SO(3) and unitary matrices.
    The following conversions are implemented and tested:
      1) SU(2)   <-> SO(3)
      2) SU(2)   <-> H (quaternions, more specifically normalised quaternions)
      3) SU(2)   <-  unitary matrices.

---
+++ qtoolkit/maths/matrix/su2/transformations.py
@@ -0,0 +1,150 @@
+# ======================================================================
+# Copyright CERFACS (October 2018)
+# Contributor: Adrien Suau (suau@cerfacs.fr)
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding  by the  rules of  distribution of free software. You can use,
+# modify  and/or  redistribute  the  software  under  the  terms  of the
+# CeCILL-B license as circulated by CEA, CNRS and INRIA at the following
+# URL "http://www.cecill.info".
+#
+# As a counterpart to the access to  the source code and rights to copy,
+# modify and  redistribute granted  by the  license, users  are provided
+# only with a limited warranty and  the software's author, the holder of
+# the economic rights,  and the  successive licensors  have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using, modifying and/or  developing or reproducing  the
+# software by the user in light of its specific status of free software,
+# that  may mean  that it  is complicated  to manipulate,  and that also
+# therefore  means that  it is reserved for  developers and  experienced
+# professionals having in-depth  computer knowledge. Users are therefore
+# encouraged  to load and  test  the software's  suitability as  regards
+# their  requirements  in  conditions  enabling  the  security  of their
+# systems  and/or  data to be  ensured and,  more generally,  to use and
+# operate it in the same conditions as regards security.
+#
+# The fact that you  are presently reading this  means that you have had
+# knowledge of the CeCILL-B license and that you accept its terms.
+# ======================================================================
+
+"""Transformations related to SU(2) matrices."""
+
+import numpy
+
+import qtoolkit.utils.constants as qconsts
+import qtoolkit.utils.types as qtypes
+
+
+def su2_to_so3(unitary: qtypes.SU2Matrix) -> qtypes.SO3Vector:
+    r"""Convert a SU(2) matrix into x in e^{-ix.sigma/2}.
+
+    This function is based on the formula:
+    $$e^{ia (\hat{n} \dot \vec{\sigma})} =
+    \cos(a) I + i(\hat{n} \dot \vec{\sigma}) \sin(a)$$
+    from https://en.wikipedia.org/wiki/Pauli_matrices
+
+    :param unitary: The SU(2) matrix to convert.
+    :return: 3 real numbers parametrising entirely the given matrix.
+    """
+    # The coefficients are: $[-\sin(a) n_1, -\sin(a) n_2, -\sin(a) n_3]$.
+    # The minus appears because there is a minus in the exponential we
+    # convert to, which is not present in the formula given in the link.
+    coefficients = numpy.array(
+        [-numpy.imag(unitary[0, 1]), numpy.real(unitary[1, 0]),
+         numpy.imag((unitary[1, 1] - unitary[0, 0]) / 2.0)])
+
+    cos_theta_2 = numpy.real((unitary[0, 0] + unitary[1, 1]) / 2.0)
+    # The vector $\hat{n}$ is supposed to be of unit-length so its norm
+    # is 1. That is why the expression below gives the sinus.
+    sin_theta_2 = numpy.linalg.norm(coefficients, 2)
+
+    if sin_theta_2 == 0.0:
+        # In the original implementation by Dawson
+        # https://github.com/cmdawson/sk/blob/master/src/su2.cpp#L152
+        # he computes theta = 2 * arccos(cos_theta_2). This seems to be
+        # useless as we know that theta = 2*k*pi.
+        # So we don't need to compute theta and we fix it to 2*pi.
+        coefficients = numpy.array([2 * numpy.pi, 0.0, 0.0])
+    else:
+        # We return the vector of coefficients, not normalised.
+        theta = 2 * numpy.arctan2(sin_theta_2, cos_theta_2)
+        coefficients = theta * coefficients / sin_theta_2
+
+    return coefficients
+
+
+def so3_to_su2(coefficients: qtypes.SO3Vector) -> qtypes.SU2Matrix:
+    r"""Convert a set of 3 real coefficients to a unique SU(2) matrix.
+
+    Computes the unitary matrix in SU(2) with the formula
+    $$e^{ia (\hat{n} \dot \vec{\sigma})} =
+    \cos(a) I + i(\hat{n} \dot \vec{\sigma}) \sin(a)$$
+    from https://en.wikipedia.org/wiki/Pauli_matrices
+
+    :param coefficients: 3 real numbers characterising the SU(2) matrix.
+    :return: The SU(2) matrix characterised by the given coefficients.
+    """
+    theta = numpy.linalg.norm(coefficients, 2)
+    identity = numpy.identity(2)
+
+    if theta == 0.0:
+        return identity
+    else:
+        normalised_coefficients = coefficients / theta
+        theta_2 = theta / 2
+        sin_theta_2 = numpy.sin(theta_2)
+        unitary = (numpy.cos(theta_2) * identity - 1.j * sin_theta_2 * (
+            normalised_coefficients[0] * qconsts.P_X + normalised_coefficients[
+            1] * qconsts.P_Y + normalised_coefficients[2] * qconsts.P_Z))
+        return unitary
+
+
+def su2_to_H(unitary: qtypes.SU2Matrix) -> numpy.ndarray:
+    """Convert a SU(2) matrix to a unit-norm quaternion.
+
+    Quaternions are composed of 4 real numbers. These numbers corresponds to
+    the coefficients $\alpha_i$ in the equation
+    $$e^{ia (\hat{n} \dot \vec{\sigma})} =
+    \alpha_0 I + i(\hat{\alpha} \dot \vec{\sigma})$$
+    from https://en.wikipedia.org/wiki/Pauli_matrices
+
+    :param unitary: The unitary matrix to decompose.
+    :return: The 4 real coefficients of the unit-norm quaternion representing
+    the matrix.
+    """
+    coefficients = numpy.array(
+        [numpy.real(unitary[0, 0]), -numpy.imag(unitary[0, 1]),
+         numpy.real(unitary[1, 0]), numpy.imag(unitary[1, 1])])
+    return coefficients
+
+
+def H_to_su2(coefficients: numpy.ndarray) -> qtypes.SU2Matrix:
+    """Convert a unit-norm quaternion to a SU(2) matrix.
+
+    Quaternions are composed of 4 real numbers. These numbers corresponds to
+    the coefficients $\alpha_i$ in the equation
+    $$e^{ia (\hat{n} \dot \vec{\sigma})} =
+    \alpha_0 I + i(\hat{\alpha} \dot \vec{\sigma})$$
+    from https://en.wikipedia.org/wiki/Pauli_matrices
+
+    :param coefficients: The coefficients characterising the SU(2) matrix.
+    :return: The SU(2) matrix corresponding to the given coefficients.
+    """
+    return coefficients[0] * qconsts.ID2 - 1.j * (
+        coefficients[1] * qconsts.P_X + coefficients[2] * qconsts.P_Y +
+        coefficients[3] * qconsts.P_Z)
+
+
+def unitary_to_su2(unitary: qtypes.UnitaryMatrix) -> qtypes.SU2Matrix:
+    """Project the given unitary matrix in SU(2).
+
+    This routine just scales the given unitary to make its determinant equals to
+    1.
+
+    :param unitary: The unitary matrix to project.
+    :return: The corresponding SU(2) matrix.
+    """
+    det = unitary[0, 0] * unitary[1, 1] - unitary[0, 1] * unitary[1, 0]
+    return unitary / numpy.lib.scimath.sqrt(det)
---
+++ tests/maths/matrix/su2/transformations.py
@@ -0,0 +1,241 @@
+# ======================================================================
+# Copyright CERFACS (October 2018)
+# Contributor: Adrien Suau (suau@cerfacs.fr)
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding  by the  rules of  distribution of free software. You can use,
+# modify  and/or  redistribute  the  software  under  the  terms  of the
+# CeCILL-B license as circulated by CEA, CNRS and INRIA at the following
+# URL "http://www.cecill.info".
+#
+# As a counterpart to the access to  the source code and rights to copy,
+# modify and  redistribute granted  by the  license, users  are provided
+# only with a limited warranty and  the software's author, the holder of
+# the economic rights,  and the  successive licensors  have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using, modifying and/or  developing or reproducing  the
+# software by the user in light of its specific status of free software,
+# that  may mean  that it  is complicated  to manipulate,  and that also
+# therefore  means that  it is reserved for  developers and  experienced
+# professionals having in-depth  computer knowledge. Users are therefore
+# encouraged  to load and  test  the software's  suitability as  regards
+# their  requirements  in  conditions  enabling  the  security  of their
+# systems  and/or  data to be  ensured and,  more generally,  to use and
+# operate it in the same conditions as regards security.
+#
+# The fact that you  are presently reading this  means that you have had
+# knowledge of the CeCILL-B license and that you accept its terms.
+# ======================================================================
+
+"""Tests for the transformation.py file."""
+
+import typing
+import unittest
+
+import numpy
+import scipy
+
+import qtoolkit.maths.matrix.generation.su2 as gen_su2
+import qtoolkit.maths.matrix.generation.unitary as gen_unitary
+import qtoolkit.maths.matrix.su2.transformations as trans
+import qtoolkit.maths.random as rand
+import qtoolkit.utils.constants as qconst
+import tests.qtestcase as qtest
+
+
+class TransformationsSU2SO3TestCase(qtest.QTestCase):
+    """Tests for the transformations between SO(3) and SU(2)."""
+
+    def test_su2_to_so3_identity(self) -> None:
+        """Tests su2_to_so3 for multiples of the identity matrix."""
+        expected_coefficients = numpy.array([2 * numpy.pi, 0.0, 0.0])
+        for coefficient in numpy.linspace(-1, 1, 100):
+            result = trans.su2_to_so3(coefficient * qconst.IDENTITY_2X2)
+            self.assert2NormClose(expected_coefficients, result)
+
+    def test_su2_to_so3_pauli_x(self) -> None:
+        """Tests su2_to_so3 for the Pauli X matrix."""
+        expected_coefficients = numpy.array([1.0, 0.0, 0.0])
+        result = trans.su2_to_so3(scipy.linalg.expm(-1.j * qconst.P_X / 2))
+        self.assert2NormClose(expected_coefficients, result)
+
+    def test_su2_to_so3_pauli_y(self) -> None:
+        """Tests su2_to_so3 for the Pauli Y matrix."""
+        expected_coefficients = numpy.array([0.0, 1.0, 0.0])
+        result = trans.su2_to_so3(scipy.linalg.expm(-1.j * qconst.P_Y / 2))
+        self.assert2NormClose(expected_coefficients, result)
+
+    def test_su2_to_so3_pauli_z(self) -> None:
+        """Tests su2_to_so3 for the Pauli Z matrix."""
+        expected_coefficients = numpy.array([0.0, 0.0, 1.0])
+        result = trans.su2_to_so3(scipy.linalg.expm(-1.j * qconst.P_Z / 2))
+        self.assert2NormClose(expected_coefficients, result)
+
+    def test_so3_to_su2_all_zero(self) -> None:
+        """Tests so3_to_su2 for a zero SO(3) vector."""
+        matrix = trans.so3_to_su2(numpy.array([0.0, 0.0, 0.0]))
+        self.assert2NormClose(matrix, matrix[0, 0] * numpy.identity(2))
+
+    def test_so3_to_su2_pauli_x(self) -> None:
+        """Tests so3_to_su2 for a vector representing the Pauli X matrix."""
+        matrix = trans.so3_to_su2(numpy.array([1.0, 0.0, 0.0]))
+        self.assert2NormClose(matrix, scipy.linalg.expm(-1.j * qconst.P_X / 2))
+
+    def test_so3_to_su2_pauli_y(self) -> None:
+        """Tests so3_to_su2 for a vector representing the Pauli Y matrix."""
+        matrix = trans.so3_to_su2(numpy.array([0.0, 1.0, 0.0]))
+        self.assert2NormClose(matrix, scipy.linalg.expm(-1.j * qconst.P_Y / 2))
+
+    def test_so3_to_su2_pauli_z(self) -> None:
+        """Tests so3_to_su2 for a vector representing the Pauli Z matrix."""
+        matrix = trans.so3_to_su2(numpy.array([0.0, 0.0, 1.0]))
+        self.assert2NormClose(matrix, scipy.linalg.expm(-1.j * qconst.P_Z / 2))
+
+    def test_so3_to_su2_to_so3_random(self) -> None:
+        """Tests X == su2_to_so3(so3_to_su2(X)) for random X."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+        for idx in range(qconst.RANDOM_SAMPLES):
+            local_coefficients = numpy.random.rand(3)
+            self.assert2NormClose(
+                trans.su2_to_so3(trans.so3_to_su2(local_coefficients)),
+                local_coefficients)
+
+    def test_su2_to_so3_to_su2_random(self) -> None:
+        """Tests X == so3_to_su2(su2_to_so3(X)) for random X."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+        for idx in range(qconst.RANDOM_SAMPLES):
+            local_su2 = gen_su2.generate_random_SU2_matrix()
+            self.assert2NormClose(trans.so3_to_su2(trans.su2_to_so3(local_su2)),
+                                  local_su2)
+
+
+class TransformationsSU2HTestCase(qtest.QTestCase):
+    """Tests for the transformations between SU(2) and H (quaternions)."""
+
+    def test_H_to_su2_identity(self) -> None:
+        """Tests H_to_su2 for the identity matrix."""
+        mat = trans.H_to_su2(numpy.array([1, 0, 0, 0]))
+        self.assertOperatorNormClose(mat, qconst.ID2_SU2)
+
+    def test_H_to_su2_pauli_x(self) -> None:
+        """Tests H_to_su2 for the Pauli X matrix."""
+        mat = trans.H_to_su2(numpy.array([0, 1, 0, 0]))
+        self.assertOperatorNormClose(mat, qconst.P_X_SU2)
+
+    def test_H_to_su2_pauli_y(self) -> None:
+        """Tests H_to_su2 for the Pauli Y matrix."""
+        mat = trans.H_to_su2(numpy.array([0, 0, 1, 0]))
+        self.assertOperatorNormClose(mat, qconst.P_Y_SU2)
+
+    def test_H_to_su2_pauli_z(self) -> None:
+        """Tests H_to_su2 for the Pauli Z matrix."""
+        mat = trans.H_to_su2(numpy.array([0, 0, 0, 1]))
+        self.assertOperatorNormClose(mat, qconst.P_Z_SU2)
+
+    def test_su2_to_H_identity(self) -> None:
+        """Tests su2_to_H for the identity matrix."""
+        quaternion = trans.su2_to_H(qconst.ID2_SU2)
+        self.assert2NormClose(quaternion, numpy.array([1, 0, 0, 0]))
+
+    def test_su2_to_H_pauli_x(self) -> None:
+        """Tests su2_to_H for the Pauli X matrix."""
+        quaternion = trans.su2_to_H(qconst.P_X_SU2)
+        self.assert2NormClose(quaternion, numpy.array([0, 1, 0, 0]))
+
+    def test_su2_to_H_pauli_y(self) -> None:
+        """Tests su2_to_H for the Pauli Y matrix."""
+        quaternion = trans.su2_to_H(qconst.P_Y_SU2)
+        self.assert2NormClose(quaternion, numpy.array([0, 0, 1, 0]))
+
+    def test_su2_to_H_pauli_z(self) -> None:
+        """Tests su2_to_H for the Pauli Z matrix."""
+        quaternion = trans.su2_to_H(qconst.P_Z_SU2)
+        self.assert2NormClose(quaternion, numpy.array([0, 0, 0, 1]))
+
+    def test_su2_to_H_to_su2_random(self) -> None:
+        """Tests X == H_to_su2(su2_to_H(X)) for random X."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+        for idx in range(qconst.RANDOM_SAMPLES):
+            local_su2 = gen_su2.generate_random_SU2_matrix()
+            self.assertOperatorNormClose(
+                trans.H_to_su2(trans.su2_to_H(local_su2)), local_su2)
+
+    def test_H_to_su2_to_H_random(self) -> None:
+        """Tests X == su2_to_H(H_to_su2(X)) for random X."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+        for idx in range(qconst.RANDOM_SAMPLES):
+            quaternion = rand.generate_unit_norm_quaternion()
+            self.assert2NormClose(trans.su2_to_H(trans.H_to_su2(quaternion)),
+                                  quaternion)
+
+
+class TransformationsSU2UnitTestCase(qtest.QTestCase):
+    """Tests for the transformations between SU(2) and unitary matrices."""
+
+    def test_unitary_to_su2_identity(self) -> None:
+        """Test validity of unitary_to_su2 for the identity matrix."""
+        self.assertOperatorNormClose(qconst.ID2,
+                                     trans.unitary_to_su2(qconst.ID2))
+
+    def test_unitary_to_su2_pauli_x(self) -> None:
+        """Test validity of unitary_to_su2 for the Pauli X matrix."""
+        self.assertOperatorNormClose(qconst.P_X_SU2,
+                                     trans.unitary_to_su2(qconst.P_X))
+
+    def test_unitary_to_su2_pauli_y(self) -> None:
+        """Test validity of unitary_to_su2 for the Pauli Y matrix."""
+        self.assertOperatorNormClose(qconst.P_Y_SU2,
+                                     trans.unitary_to_su2(qconst.P_Y))
+
+    def test_unitary_to_su2_pauli_z(self) -> None:
+        """Test validity of unitary_to_su2 for the Pauli Z matrix."""
+        self.assertOperatorNormClose(qconst.P_Z_SU2,
+                                     trans.unitary_to_su2(qconst.P_Z))
+
+    def test_unitary_to_su2_repeated_random(self) -> None:
+        """Test that repetition of unitary_to_su2 has no effect."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+
+        ReturnType = typing.TypeVar("ReturnType")
+
+        def repeat_func(f: typing.Callable[[ReturnType], ReturnType], n: int,
+                        args: ReturnType) -> ReturnType:
+            for i in range(n):
+                args = f(args)
+            return args
+
+        for idx in range(qconst.RANDOM_SAMPLES):
+            unitary = gen_unitary.generate_random_unitary_matrix()
+            # We will repeat the application of unitary_to_su2 up to 6 times.
+            # 6 has been arbitrarily chosen.
+            su2_matrix = trans.unitary_to_su2(unitary)
+            for i in range(6):
+                self.assertOperatorNormClose(
+                    repeat_func(trans.unitary_to_su2, i, su2_matrix),
+                    su2_matrix)
+
+    def test_unitary_to_su2_random(self) -> None:
+        """Tests det(unitary_to_su2(X)) == 1 for random unitary X."""
+        # Abort if we don't want random tests
+        if not qconst.USE_RANDOM_TESTS:
+            return
+        for idx in range(qconst.RANDOM_SAMPLES):
+            unitary = gen_unitary.generate_random_unitary_matrix()
+            self.assertAlmostEqual(
+                numpy.linalg.det(trans.unitary_to_su2(unitary)), 1.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
