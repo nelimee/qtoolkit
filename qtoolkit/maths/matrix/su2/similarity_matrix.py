f6a5568876e7431658f4d926b58c53a2feb03de4
Adrien Suau <adrien.suau@cerfacs.fr>
2018-10-17 10:17:15 +0200

    Implementation of the similarity_matrix algorithm for SU(2).
    The algorithm implemented is the one implemented in the GitHub of C. Dawson:
        - https://github.com/cmdawson/sk/blob/master/src/su2.cpp#L77
    and in:
        - https://github.com/cryptogoth/skc-python/blob/master/skc/dawson/factor.py#L10
    The major drawback with these implementations was the lack of comments
    to explain the code. This issue is not addressed in this code. I still
    need to understand the algorithm used.
    Tests are missing, I need to figure out the input space of this method
    because it does not seem to work for arbitrary matrices, but still it
    gives good results within the Solovay-Kitaev algorithm.

---
+++ qtoolkit/maths/matrix/su2/similarity_matrix.py
@@ -0,0 +1,62 @@
+# ======================================================================
+# Copyright CERFACS (October 2018)
+# Contributor: Adrien Suau (suau@cerfacs.fr)
+#
+# This software is governed by the CeCILL-B license under French law and
+# abiding  by the  rules of  distribution of free software. You can use,
+# modify  and/or  redistribute  the  software  under  the  terms  of the
+# CeCILL-B license as circulated by CEA, CNRS and INRIA at the following
+# URL "http://www.cecill.info".
+#
+# As a counterpart to the access to  the source code and rights to copy,
+# modify and  redistribute granted  by the  license, users  are provided
+# only with a limited warranty and  the software's author, the holder of
+# the economic rights,  and the  successive licensors  have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading,  using, modifying and/or  developing or reproducing  the
+# software by the user in light of its specific status of free software,
+# that  may mean  that it  is complicated  to manipulate,  and that also
+# therefore  means that  it is reserved for  developers and  experienced
+# professionals having in-depth  computer knowledge. Users are therefore
+# encouraged  to load and  test  the software's  suitability as  regards
+# their  requirements  in  conditions  enabling  the  security  of their
+# systems  and/or  data to be  ensured and,  more generally,  to use and
+# operate it in the same conditions as regards security.
+#
+# The fact that you  are presently reading this  means that you have had
+# knowledge of the CeCILL-B license and that you accept its terms.
+# ======================================================================
+
+import numpy
+
+import qtoolkit.maths.matrix.su2.transformations as su2trans
+import qtoolkit.utils.types as qtypes
+
+
+def similarity_matrix(A: qtypes.SU2Matrix,
+                      B: qtypes.SU2Matrix) -> qtypes.SU2Matrix:
+    """Find S in SU(2) such that A = S @ B @ S.T.conj().
+
+    :param A: First SU(2) matrix.
+    :param B: Second SU(2) matrix.
+    :return: The SU(2) matrix S.
+    """
+    a, b = su2trans.su2_to_so3(A), su2trans.su2_to_so3(B)
+    norm_a, norm_b = numpy.linalg.norm(a, 2), numpy.linalg.norm(b, 2)
+
+    s = numpy.cross(b, a)
+    norm_s = numpy.linalg.norm(s, 2)
+
+    if norm_s == 0:
+        # The representative vectors are too close to each other, this
+        # means that the original matrices are also very close, and so
+        # returning the identity matrix is fine.
+        return numpy.identity(2)
+
+    angle_between_a_and_b = numpy.arccos(numpy.inner(a, b) / (norm_a * norm_b))
+    s *= angle_between_a_and_b / norm_s
+
+    S = su2trans.so3_to_su2(s)
+    return S
