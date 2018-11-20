50a4781198e49490daaf314dde28073b2c4b4230
Adrien Suau <adrien.suau@cerfacs.fr>
2018-11-20 15:14:15 +0100

    Moved Sphinx to docs/

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+# Make sure that the module is in the PYTHONPATH.
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,6 +30,7 @@ version = ''
 # The full version, including alpha/beta/rc tags
 release = ''
 
+
 # -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -37,11 +40,12 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode', ]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
+    'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 'sphinx.ext.githubpages',
+    'sphinx_autodoc_typehints', ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -62,17 +66,18 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['.build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -83,7 +88,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -101,9 +106,11 @@ html_static_path = ['.static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'qtoolkitdoc'
 
+
 # -- Options for LaTeX output ------------------------------------------------
 
-latex_elements = {  # The paper size ('letterpaper' or 'a4paper').
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
 
@@ -124,8 +131,9 @@ latex_elements = {  # The paper size ('letterpaper' or 'a4paper').
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [(
-    master_doc, 'qtoolkit.tex', 'qtoolkit Documentation', 'Adrien Suau',
-    'manual'), ]
+master_doc, 'qtoolkit.tex', 'qtoolkit Documentation', 'Adrien Suau',
+'manual'), ]
+
 
 # -- Options for manual page output ------------------------------------------
 
@@ -133,14 +141,16 @@ latex_documents = [(
 # (source start file, name, description, authors, manual section).
 man_pages = [(master_doc, 'qtoolkit', 'qtoolkit Documentation', [author], 1)]
 
+
 # -- Options for Texinfo output ----------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [(
-    master_doc, 'qtoolkit', 'qtoolkit Documentation', author, 'qtoolkit',
-    'One line description of project.', 'Miscellaneous'), ]
+master_doc, 'qtoolkit', 'qtoolkit Documentation', author, 'qtoolkit',
+'One line description of project.', 'Miscellaneous'), ]
+
 
 # -- Options for Epub output -------------------------------------------------
 
