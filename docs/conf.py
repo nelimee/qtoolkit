72a6a68d7f36e7878f303fec6805876b0fa545bf
Adrien Suau <adrien.suau@cerfacs.fr>
2018-11-22 09:02:42 +0100

    Added 2 options in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,30 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'scripts/*']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = False
+
+# A list of prefixes that are ignored for sorting the Python module index (e.g.,
+# if this is set to['foo.'], then foo.bar is shown under B, not F). This can be
+# handy if you document a project that consists of a single package. Works only
+# for the HTML builder currently. Default is [].
+# New in version 0.6.
+# modindex_common_prefix = ['qtoolkit.']
+
+# This value selects what content will be inserted into the main body of an
+# autoclass directive. The possible values are:
+# - "class":
+#     Only the class’ docstring is inserted. This is the default. You can still
+#     document __init__ as a separate method using automethod or the members
+#     option to autoclass.
+# - "both":
+#     Both the class’ and the __init__ method’s docstring are concatenated and
+#     inserted.
+# - "init":
+#     Only the __init__ method’s docstring is inserted.
+autoclass_content = 'init'
+
 
 # -- Options for HTML output -------------------------------------------------
 
